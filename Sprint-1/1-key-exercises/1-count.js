a4b3d247c6e90967b1ac46a66d56207aa83148cb
Tesfalem Hailu <hailutesfalem21@gmail.com>
2025-06-21 11:51:42 +0100

    change

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-count = count + 1;
+count = count + 2;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
